109f2804e87808548b447e33cb161357ddddeb42
WellingtonGabriel1 <WellingtonGabriel7700@gmail.com>
2023-08-19 09:47:00 -0300

    initial commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,8 @@
+import './index.css'
+
+export const Button = ({ children, onClick }) => {
+    return (
+    <button onClick={onClick} className="button-container">
+        {children}</button>
+        );
+};
---
+++ src/screens/Home/index.js
@@ -0,0 +1,44 @@
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react'
+import "./index.css";
+import { Button, Input } from '../../components';
+
+export const HomeScreen = () => {
+    const navigate = useNavigate();
+    const [Username, setUsername] = useState("");
+
+    const onClickContinue = () => {
+        if(Username.length < 3) {
+            alert("Username deve conter mais do que 3 caracteres!");
+            return;
+        };
+
+        localStorage.setItem("@supermarket-list:username", Username);
+        navigate("/List");
+    };
+
+    return <div className='home-screen-container'>
+        <div className='home-screen-content-container'>
+            <img className='shopping-bag-image' src='/images/shopping-bag.svg' alt='shopping-bag'/>
+
+            <h1 className='home-screen-title'>
+                Sua lista de supermercado mais fácil do que nunca
+            </h1>
+            <h3 className='home-screen-subtitle'>
+                Ajudamos você a organizar sua lista de compras de forma descomplicada.
+            </h3>
+            <h3 className='home-screen-subtitle-description'>
+                Digite abaixo seu usuário para ter acesso a sua lista de compras:
+            </h3>
+            
+            <Input onChange={(text) =>setUsername(text)}
+            value={Username}
+            label='Username'
+            placeholder="Ex: usuario1"
+            />
+            <div className='home-screen-button-container'>
+             <Button onClick={onClickContinue}>Continuar</Button>
+            </div>
+        </div>
+    </div>
+};
